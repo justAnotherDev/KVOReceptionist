4ea8075c318b0ba49ea5ff2f0078385b4822a925
justAnotherDev <cwe7976@gmail.com>
2016-04-15 01:56:14 -0700

    add optional param for setting which thread to call the handler on

--- KVOReceptionist/KVOReceptionist.swift
+++ KVOReceptionist/KVOReceptionist.swift
@@ -1,9 +1,23 @@
 class KVOReceptionist<T>: NSObject {
     
-    init(keyPath: String, object: AnyObject, handler: (oldValue: T?, newValue: T?) -> Void) {
+    init(keyPath: String, object: AnyObject, queue: dispatch_queue_t = dispatch_get_main_queue(), handler: (oldValue: T?, newValue: T?) -> Void) {
+        
+        let handleOnMainThread = dispatch_queue_get_label(dispatch_get_main_queue()) == dispatch_queue_get_label(queue)
+        
         // closure to parse the KVO response and return the old/new values to the handler
         kvoHandler = { change in
-            handler(oldValue: change?[NSKeyValueChangeOldKey] as? T, newValue: change?[NSKeyValueChangeNewKey] as? T)
+            // locally redefine handler as Void -> Void
+            let handler = { handler(oldValue: change?[NSKeyValueChangeOldKey] as? T, newValue: change?[NSKeyValueChangeNewKey] as? T) }
+            
+            // dispatch handler to the correct queue
+            switch (handleOnMainThread, NSThread.isMainThread()) {
+            case (true, true):
+                handler() // main thread dispatch and already on main thread. call immediately
+            case (true, false):
+                dispatch_async(queue, handler) // main thread dispatch and not on main thread. dispatch async
+            default:
+                dispatch_sync(queue, handler) // background thread dispatch. dispatch sync
+            }
         }
         
         // closure to remove KVO observer during deinit
